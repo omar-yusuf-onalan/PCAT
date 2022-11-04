e861973b07cb61c835f49d84f97a433474661824
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-11-05 01:18:46 +0300

    Pagination implemented

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,9 +2,19 @@ const fs = require('fs');
 const Photo = require('../models/Photo');
 
 exports.getAllPhotos = async (req, res) => {
-    const photos = await Photo.find({}).sort('-dateCreated');
+    const page = req.query.page || 1;
+    const photosPerPage = 2;
+
+    const totalPhotos = await Photo.find().countDocuments();
+    const photos = await Photo.find({})
+        .sort('-dateCreated')
+        .skip((page - 1) * photosPerPage)
+        .limit(photosPerPage);
+
     res.render('index', {
-        photos,
+        photos: photos,
+        current: page,
+        pages: Math.ceil(totalPhotos / photosPerPage),
     });
 };
 
@@ -47,7 +57,7 @@ exports.updatePhoto = async (req, res) => {
 exports.deletePhoto = async (req, res) => {
     const photo = await Photo.findOne({ _id: req.params.id });
     let deletedImage = __dirname + '/../public' + photo.image;
-    if (deletedImage.includes('/') == true) {
+    if (deletedImage === `${__dirname}/public/${photo.image}`) {
         fs.unlinkSync(deletedImage);
         await Photo.findByIdAndRemove(req.params.id);
         res.redirect('/');
