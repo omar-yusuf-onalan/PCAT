7ed03cc15fcfde95198cc78b746667d22fbae98d
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2022-11-05 16:13:49 +0300

    Ready for Heroku

--- app.js
+++ app.js
@@ -14,10 +14,20 @@ const app = express();
 
 //connect DB
 
-mongoose.connect('mongodb://localhost/pcat-test-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(
+        'mongodb+srv://omaryusufonalan:suSlzojNUtlxCsuz@pcat-app.hbfhaog.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log('DB CONNECTED');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -44,7 +54,7 @@ app.get('/about', pageController.getAboutPage);
 app.get('/add', pageController.getAddPage);
 app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server initialized in port ${port}...`);
